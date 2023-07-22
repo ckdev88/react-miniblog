27cb9efd39b5b8c659623f5a9c8dd663336d9cef
ckdev88 <ckdev88@outlook.com>
2023-07-22 14:47:54 -0300

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+// import logo from './logo.svg';
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="text-1xl font-bold underline">
+					TailwindCSS geinstalleerd
+				</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
