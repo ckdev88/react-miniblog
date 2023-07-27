f4b2e451573545dd8af1049246e4d70483b73f85
ckdev88 <ckdev88@outlook.com>
2023-07-27 14:15:52 -0300

    add age prediction & many small features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-// import logo from './logo.svg';
 import "./App.css";
+import { useState, useEffect } from "react";
+import Agefetch from "./components/Agefetch";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1 className="text-1xl font-bold underline">
-					TailwindCSS geinstalleerd
+				<h1 className="text-3xl overline flex-shrink mb-10 uppercase font-extrabold border-t-4">
+					Aloha
 				</h1>
+				<Agefetch />
 			</header>
 		</div>
 	);
