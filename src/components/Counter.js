1cb2e4d71975c077d6af2c38ba1b11da403b14f9
ckdev88 <ckdev88@outlook.com>
2023-07-27 21:48:13 -0300

    minify counter button application a bit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,35 +2,31 @@ import { useState } from "react";
 
 export default function Counter() {
 	const [counter, setCounter] = useState(0);
-
-	const increaseCount = () => {
-		setCounter(counter + 1);
-	};
-	const decreaseCount = () => {
-		setCounter(counter - 1);
-	};
-	const resetCount = () => {
-		setCounter(0);
-	};
 	return (
 		<div>
 			<h1 className="text-2xl mb-3">Counter!</h1>
 			<button
-				onClick={increaseCount}
+				onClick={() => {
+					setCounter(counter + 1);
+				}}
 				className="rounded-md bg-green-600 py-1 px-1.5 mb-2"
 			>
 				Plus
 			</button>
 			<br />
 			<button
-				onClick={decreaseCount}
+				onClick={() => {
+					setCounter(counter - 1);
+				}}
 				className="rounded-md bg-red-600 py-1 px-1.5 mb-2"
 			>
 				Minus
 			</button>
 			<br />
 			<button
-				onClick={resetCount}
+				onClick={() => {
+					setCounter(0);
+				}}
 				className="rounded-md bg-black py-1 px-1.5 mb-2"
 			>
 				Reset
