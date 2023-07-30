5308f77cfacbaee91a0949666314dbd32404f2bf
ckdev88 <ckdev88@outlook.com>
2023-07-30 02:35:07 -0300

    apply react-query (tanstack) & axios for data fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Pages from "./components/Pages";
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
 
 function App() {
+	const client = new QueryClient();
 	const [username, setUsername] = useState("Anonymous");
 	return (
-		<AppContext.Provider value={{ username, setUsername }}>
+		<QueryClientProvider client={client}>
 			<div className="App bg-zinc-200 h-100">
 				<header className="App-header py-4 mb-0 my-auto">
 					<strong className="text-3xl overline mb-2 uppercase font-extrabold block">
@@ -16,7 +18,7 @@ function App() {
 				</header>
 				<Pages />
 			</div>
-		</AppContext.Provider>
+		</QueryClientProvider>
 	);
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import { useContext } from "react";
-import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Home() {
-	const { username } = useContext(AppContext);
-	return <h1>Aloha {username}!</h1>;
+	const {
+		data: catData,
+		isLoading,
+		isError,
+		refetch,
+	} = useQuery(["cat"], () => {
+		return axios.get("https://catfact.ninja/fact").then((res) => res.data);
+	});
+
+	console.log({ catData });
+	// const { username } = useContext(AppContext);
+	return (
+		<main>
+			<h1>Aloha!</h1>
+
+			<h2>Cat fact of the day</h2>
+			{isLoading && <p>...</p>}
+			{isError && <p>Oeps, foutje?</p>}
+			<p>{catData?.fact}</p>
+			<button onClick={refetch}>Again!</button>
+		</main>
+	);
 }
