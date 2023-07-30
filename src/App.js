67a84a6cbb4c67a5c566d7623a5efa6247468f94
ckdev88 <ckdev88@outlook.com>
2023-07-30 11:58:46 -0300

    fix Profile page which still uses createContext() component

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ function App() {
 	const [username, setUsername] = useState("Anonymous");
 	return (
 		<QueryClientProvider client={client}>
-			<div className="App bg-zinc-200 h-100">
-				<header className="App-header py-4 mb-0 my-auto">
-					<strong className="text-3xl overline mb-2 uppercase font-extrabold block">
-						Aloha!
-					</strong>
-				</header>
-				<Pages />
-			</div>
+			<AppContext.Provider value={{ username, setUsername }}>
+				<div className="App bg-zinc-200 h-100">
+					<header className="App-header py-4 mb-0 my-auto">
+						<strong className="text-3xl overline mb-2 uppercase font-extrabold block">
+							Aloha!
+						</strong>
+					</header>
+					<Pages />
+				</div>
+			</AppContext.Provider>
 		</QueryClientProvider>
 	);
 }
