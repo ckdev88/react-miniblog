78186e77149f2379b76c689e0829d7ac5b1e576a
ckdev88 <ckdev88@outlook.com>
2023-07-31 18:58:25 -0300

    make custom hook for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,29 @@
-import { useState } from "react";
+import useCounter from "./hooks/useCounter";
 
-export default function Counter() {
-	const [counter, setCounter] = useState(0);
+const Counter = () => {
+	const { counter, plus, minus, reset } = useCounter(1);
 	return (
 		<div>
 			<h2 className="text-2xl mb-3">Counter!</h2>
 			<button
-				onClick={() => {
-					setCounter(counter + 1);
-				}}
+				onClick={plus}
 				className="rounded-md bg-green-600 py-1 px-1.5 mb-2"
 			>
 				Plus
 			</button>
 			<br />
 			<button
-				onClick={() => {
-					setCounter(counter - 1);
-				}}
+				onClick={minus}
 				className="rounded-md bg-red-600 py-1 px-1.5 mb-2"
 			>
 				Minus
 			</button>
 			<br />
-			<button
-				onClick={() => {
-					setCounter(0);
-				}}
-				className="rounded-md bg-black py-1 px-1.5 mb-2"
-			>
+			<button onClick={reset} className="rounded-md bg-black py-1 px-1.5 mb-2">
 				Reset
 			</button>
 			<p className="text-xl">{counter}</p>
 		</div>
 	);
-}
+};
+export default Counter;
